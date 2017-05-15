550f4deb78ca53e8912ddb647630b6c919721e60
DuShaun Alderson-Claeys <dushaun.ac@gmail.com>
2017-05-15 23:20:31 +0100

    routes edit

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post(
+    'stripe/webhook',
+    '\Laravel\Cashier\Http\Controllers\WebhookController@handleWebhook'
+);
