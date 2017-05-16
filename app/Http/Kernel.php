1be7574c8a3c95e63f58ae13a1a64212a1a02a28
DuShaun Alderson-Claeys <dushaun.ac@gmail.com>
2017-05-16 15:59:23 +0100

    updating kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'subscribed' => \App\Http\Middleware\Subscribed::class,
-        'subscriber.premium' => \App\Http\Middleware\PremiumSubscriber::class,
+        'subscribed.premium' => \App\Http\Middleware\PremiumSubscriber::class,
     ];
 }
