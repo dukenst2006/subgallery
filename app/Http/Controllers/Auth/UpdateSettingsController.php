7a33da5d5a6b48730d22ce3672622baa6f3ddd58
DuShaun Alderson-Claeys <dushaun.ac@gmail.com>
2017-05-18 23:01:11 +0100

    changing authenticated user call

--- app/Http/Controllers/Auth/UpdateSettingsController.php
+++ app/Http/Controllers/Auth/UpdateSettingsController.php
@@ -6,15 +6,16 @@ use App\Http\Requests\UpdateUsername;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class UpdateSettingsController extends Controller
 {
     public function username(UpdateUsername $request)
     {
-        $authUser = $request->user();
+        $authUser = Auth::user();
         $newUsername = $request->username;
 
-        $id = $authUser['id'];
+        $id = $authUser->id;
         $user = User::find($id)->first();
         $user->username = $newUsername;
         $user->save();
