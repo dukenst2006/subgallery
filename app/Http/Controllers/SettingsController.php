ad76c9d1385549c6a7a57c28d0198e72ae941e3a
DuShaun Alderson-Claeys <dushaun.ac@gmail.com>
2017-05-25 01:07:12 +0100

    controller changes

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -36,7 +36,7 @@ class SettingsController extends Controller
     public function invoices()
     {
         $user = User::find(Auth::id());
-        $invoices = $user->invoices();
+        $invoices = $user->invoicesIncludingPending();
         return view('settings.billing.invoices', compact('invoices'));
     }
 
