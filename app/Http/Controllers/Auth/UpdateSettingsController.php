843630c9fe4ef2e358e9e809e6853ea7f5fb08b7
DuShaun Alderson-Claeys <dushaun.ac@gmail.com>
2017-05-25 14:01:19 +0100

    including auth

--- app/Http/Controllers/Auth/UpdateSettingsController.php
+++ app/Http/Controllers/Auth/UpdateSettingsController.php
@@ -9,6 +9,7 @@ use App\Plan;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UpdateSettingsController extends Controller
